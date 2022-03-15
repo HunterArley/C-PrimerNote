1f1f41c98c323f9a4006a22026a06087aa0d2143
HunterArley <18200399036@163.com>
2022-03-16 00:03:43 +0800

    书中19页及以前的代码

---
+++ sources/Unit_1/01_Test/14_Test/14_Test.cpp
@@ -0,0 +1,29 @@
+﻿// 14_Test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	/*Page18的代码
+	Sales_item book;
+	// 读入ISBN号、售出的册数以及销售价格
+	std::cin >> book;
+	// 写入ISBN、售出的册数、总销售额和平均价格
+	std::cout << book << std::endl;
+	*/
+
+	/*Page19的代码
+	Sales_item item1, item2;
+	std::cin >> item1 >> item2; // 读取一对交易记录
+	std::cout << item1 + item2 << std::endl; //打印它们的和
+	*/
+
+	/*
+	* 练习1.21
+	* 
+	*/
+    return 0;
+}
+
