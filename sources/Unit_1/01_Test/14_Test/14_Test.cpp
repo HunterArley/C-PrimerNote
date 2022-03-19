6fc1e89bd46f0a542e9ca8304d80826e94aa3f79
HunterArley <18200399036@163.com>
2022-03-20 00:22:44 +0800

    第1章中的代码和习题完成

--- sources/Unit_1/01_Test/14_Test/14_Test.cpp
+++ sources/Unit_1/01_Test/14_Test/14_Test.cpp
@@ -22,8 +22,47 @@ int main()
 
 	/*
 	* 练习1.21
-	* 
+	* 编写程序，读取两个ISBN相同的Sales_item对象，输出它们的和。
 	*/
+	/*Sales_item item1, item2;
+	std::cout << "请输入两条ISBN相同的销售记录：" << std::endl;
+	std::cin >> item1 >> item2;
+	if (compareIsbn(item1, item2))
+	{
+		std::cout << "汇总信息：ISBN、售出本数、销售额和平均售价为" << item1 + item2 << std::endl;
+	}
+	else
+	{
+		std::cout << "输入的两条ISBN不相同" << std::endl;
+	}*/
+
+	/*
+	* 练习 1.22 
+	* 编写程序，读取多个具有相同ISBN的销售记录，输出所有记录的和。
+	*/
+	Sales_item total, item;
+	std::cout << "请输入几条ISBN相同的销售记录：" << std::endl;
+	if (std::cin >> total)
+	{
+		while (std::cin >> item)
+		{
+			if (compareIsbn(total, item))
+			{
+				total = total + item;
+			}
+			else
+			{
+				std::cout << "ISBN不同" << std::endl;
+				return -1;
+			}
+		}
+		std::cout << "汇总信息：ISBN、售出本数、销售额和平均售价为：" << total << std::endl;
+	}
+	else
+	{
+		std::cout << "没有数据" << std::endl;
+		return -1;
+	}
     return 0;
 }
 
---
+++ sources/Unit_1/01_Test/16_Test/16_Test.cpp
@@ -0,0 +1,36 @@
+﻿// 16_Test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Sales_item.h"
+
+/*
+* 练习1.23
+* 编写程序，读取多条销售记录，并统计每个ISBN（每本书）有几条销售记录。
+*/
+int main()
+{
+	Sales_item item1, item2;
+	int num = 1;
+	std::cout << "请输入若干销售记录：" << std::endl;
+	if (std::cin >> item1) {
+		while (std::cin >> item2) {
+			if (compareIsbn(item1, item2)) {
+				num++; // ISBN相同计数加1
+			}
+			else
+			{
+				std::cout << item1.isbn() << "共有" << num << "条销售记录" << std::endl;
+				item1 = item2;
+				num = 1;
+			}
+		}
+		std::cout << item1.isbn() << "共有" << num << "条销售记录" << std::endl;
+	}
+	else
+	{
+		std::cout << "没有数据" << std::endl;
+		return -1;
+	}
+	return 0;
+}
